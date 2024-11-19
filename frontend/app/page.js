587e9b1563dc26c37b3e024c5ffd7b2ced1724b6
捷 <immactavish@gmail.com>
2024-11-19 13:26:29 -0500

    style: change homepage instruction text smaller on smalelr devices

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -8,8 +8,8 @@ import RecentlyViewedCollections from '@/components/RecentlyViewedCollections';
 const HomePage = () => {
   return (
     <main className="text-center py-16 ">
-      <h1 className="text-5xl font-bold text-gray-800">Master Your Vocabulary with LexiMind</h1>
-      <p className="text-lg text-gray-600 mt-4">
+      <h1 className=" text-3xl md:text-5xl font-bold text-gray-800">Master Your Vocabulary with LexiMind</h1>
+      <p className="text-sm md:text-lg text-gray-600 mt-4">
         LexiMind is your personal vocabulary-building companion. 
         <br /><br />Using an intuitive, card-based approach, we make memorizing words effortless and effective. 
         <br />Whether you're prepping for exams, learning a new language,
