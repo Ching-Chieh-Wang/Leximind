519b764dc2f1a5998449dca2c8a28c9884566e46
捷 <immactavish@gmail.com>
2024-12-03 08:03:36 -0500

    style prevent navbar colltions button overlap

--- frontend/components/Nav/LoggedInComponent.jsx
+++ frontend/components/Nav/LoggedInComponent.jsx
@@ -21,7 +21,7 @@ const LoggedInComponent = ({user}) => {
         <>
             <Link
                 href="/protected/collections"
-                className="inline-flex items-center justify-center h-10 rounded-md border border-gray-400 bg-gray-100 px-4 font-medium text-slate-900 text-xs sm:text-sm md:text-base lg:text-lg gap-x-2 hover:bg-gray-300"
+                className="inline-flex items-center justify-center h-10 rounded-md border border-gray-400 bg-gray-100 px-1 sm:px-2 md:px-3 font-medium text-slate-900 text-xs sm:text-sm md:text-base lg:text-lg gap-x-2 hover:bg-gray-300"
             >
                 <CollectionIcon />
                 Collections
