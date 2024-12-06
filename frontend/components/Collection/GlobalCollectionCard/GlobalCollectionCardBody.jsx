dc8983f91bebec8013177d2417b177149cdbb374
捷 <immactavish@gmail.com>
2024-12-06 11:46:25 -0500

    fix: search collection shows user info properly

--- frontend/components/Collection/GlobalCollectionCard/GlobalCollectionCardBody.jsx
+++ frontend/components/Collection/GlobalCollectionCard/GlobalCollectionCardBody.jsx
@@ -6,16 +6,17 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const GlobalCollectionCardBody = ({index}) => {
-    if(index==undefined){console.error("index must provide")}    const {collections}=useCollections();
-    const {username,userImage,userId}=collections[index];
+    if(index==undefined){console.error("index must provide")}    
+    const {collections}=useCollections();
+    const {username,user_image,user_id}=collections[index];
   return (
     <>
 
-    <UserCollectionCardBody/>
-    <Link className="flex items-center gap-x-4">
+    <UserCollectionCardBody index={index}/>
+    <Link href="#" className="flex items-center gap-x-4 justify-end p-3">
         <h1>Created by {username}</h1>
         <Image
-            src={user.image || '/assets/images/logo.jpg'}
+            src={user_image || '/assets/images/logo.jpg'}
             width={40}
             height={40}
             className="w-10 h-10 rounded-full cursor-pointer"
