31300cfe06d2e70f194f8a18c8ba408b7d24119b
chingchiehwang <chingchieh.wang@1thing.org>
2025-02-19 20:09:17 -0500

    fix: production issue cannnot resolve path with @

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,22 @@
 // next.config.mjs
-export default {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'lh3.googleusercontent.com',
-          pathname: '/**',
-        },
-      ],
-    },
-      reactStrictMode: true,
-  };
+import path from 'path';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+export default nextConfig;
