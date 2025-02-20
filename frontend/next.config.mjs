7eedbcc0117451a40cdd5347a0a913f8cfcbb40f
chingchiehwang <chingchieh.wang@1thing.org>
2025-02-19 20:16:34 -0500

    fix: production issue

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,9 @@
 // next.config.mjs
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix for __dirname in ES Modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
