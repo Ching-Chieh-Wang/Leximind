6e10e86d45e8fe29cd4d234f890c7174a9f36ef8
chingchiehwang <chingchieh.wang@1thing.org>
2025-02-28 13:46:10 -0500

    fix:webpack issue

--- frontend/config/c2.js
+++ frontend/config/c2.js
@@ -14,7 +14,7 @@ const c2 = new S3Client({
   forcePathStyle: true  // Required for S3-compatible storage like Synology C2
 });
 
-export const generateSignedUrl = async (fileName) => {
+const  generateSignedUrl = async (fileName) => {
   const command = new GetObjectCommand({
     Bucket: process.env.C2_BUCKET_NAME,
     Key: fileName
@@ -24,3 +24,5 @@ export const generateSignedUrl = async (fileName) => {
   const signedUrl = await getSignedUrl(c2, command, { expiresIn: 60 * 60 * 24 * 7 });
   return signedUrl;
 };
+
+export default generateSignedUrl;
